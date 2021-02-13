8ee6fa53b3f662d84cceef550486c68f2e1c396a
Fabio Fumio Wada <39932036+FabioFumioWada@users.noreply.github.com>
2021-02-13 18:07:16 -0300

    Add files via upload

---
+++ listas.py
@@ -0,0 +1,15 @@
+frutas = [
+            'abacate',
+            'laranja',
+            'banana',
+            'melão',
+            'uva',
+            'melancia',
+            'morango'
+        ]
+
+ls_frutas = [ls_frutas
+                for ls_frutas in frutas
+                if ls_frutas[0] == 'm']
+
+ls_frutas
