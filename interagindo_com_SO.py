f15f16ab64b2cb5fac26a3a8817897984cff0395
Fabio Fumio Wada <39932036+FabioFumioWada@users.noreply.github.com>
2021-02-13 18:04:54 -0300

    Add files via upload

---
+++ interagindo_com_SO.py
@@ -0,0 +1,18 @@
+import os
+
+#testar os comandos pelo prompt de comando do sistema operacional
+#pelo visual studio code o resultado não fica exato
+
+#cria diretório\pasta
+os.mkdir("c:\Teste_Pasta_Phyton")
+
+#lista diretórios
+os.listdir('.')
+
+#diretório absoluto
+os.path.abspath
+
+#executa comando do prompt
+os.system('dir/w')
+os.system('cls')
+
